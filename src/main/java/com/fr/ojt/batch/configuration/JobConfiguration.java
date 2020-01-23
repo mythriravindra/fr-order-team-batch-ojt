d1cb8f87e37ff05f610528ff09b24a3e6e26e7e7
fr-mythri <mythri.ravindra@fastretailing.com>
2020-01-23 14:40:31 +0900

    initial base application of batch

---
+++ src/main/java/com/fr/ojt/batch/configuration/JobConfiguration.java
@@ -0,0 +1,54 @@
+package com.fr.ojt.batch.configuration;
+
+import com.fr.ojt.batch.listener.ChunkListener;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableBatchProcessing
+public class JobConfiguration {
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public ItemReader<String> reader(){
+        return new ListItemReader<>(Arrays.asList("one","two","three","four","five","six","seven"));
+    }
+
+    @Bean
+    public ItemWriter<String> writer(){
+        return items -> {
+            items.stream().forEach(item -> System.out.println(item));
+        };
+    }
+    @Bean
+    public Step step() {
+        return stepBuilderFactory
+                .get("step")
+                .<String,String>chunk(2)
+                .faultTolerant()
+                .listener(new ChunkListener() )
+                .reader(reader())
+                .writer(writer())
+                .build();
+    }
+
+    @Bean
+    public Job dailyReportJob(){
+        return jobBuilderFactory.get("dailyReportJob").start(step()).build();
+    }
+}
---
+++ src/main/java/com/fr/ojt/batch/listener/ChunkListener.java
@@ -0,0 +1,18 @@
+package com.fr.ojt.batch.listener;
+
+import org.springframework.batch.core.annotation.AfterChunk;
+import org.springframework.batch.core.annotation.BeforeChunk;
+import org.springframework.batch.core.scope.context.ChunkContext;
+
+public class ChunkListener {
+
+    @BeforeChunk
+    public void beforeChunk(ChunkContext chunkContext){
+        System.out.println(">>Before chunk");
+    }
+
+    @AfterChunk
+    public void afterChunk(ChunkContext chunkContext){
+        System.out.println("<<After chunk");
+    }
+}
